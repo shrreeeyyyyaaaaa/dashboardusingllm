111f6eb06af42d99c9061f949ace5753953829c6
Shreya Alajangi <shreya8ya@gmail.com>
2025-05-18 12:40:08 +0530

    Update ext_info.py

--- agent/ext_info.py
+++ agent/ext_info.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def extract_column_info(df):
+    info = {
+        "columns": df.columns.tolist(),
+        "dtypes": df.dtypes.apply(lambda x: str(x)).to_dict(),
+        "nulls": df.isnull().sum().to_dict(),
+        "n_unique": df.nunique().to_dict()
+    }
+    return info
