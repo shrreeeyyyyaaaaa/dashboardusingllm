c974cdaa097e12794dbe606963312bc82856ff34
Shreya Alajangi <shreya8ya@gmail.com>
2025-05-18 12:42:10 +0530

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,1 +1,10 @@
+import docx
 
+def parse_requirements_file(file):
+    if file.name.endswith(".txt"):
+        return file.read().decode("utf-8")
+    elif file.name.endswith(".docx"):
+        doc = docx.Document(file)
+        return "\n".join([p.text for p in doc.paragraphs])
+    else:
+        return ""
